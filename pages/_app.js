2acaf23050ce3b84683dd6af677302dea4938b77
jagadish <goldjagadish@gmail.com>
2022-11-03 19:24:45 +0530

    added progress bar while routing to pages

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,23 @@ import '../styles/footer.css';
 import '../styles/nav.css';
 import '../styles/events.css';
 import '../styles/navigation.css';
-
+import "../styles/nprogress.css"
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+	Router.events.on('routeChangeStart', (url)=>{
+		NProgress.start();
+	})
+
+	Router.events.on('routeChangeComplete', (url)=>{
+		NProgress.done();
+	})
+	return <Component {...pageProps} />
 }
 
 export default MyApp
